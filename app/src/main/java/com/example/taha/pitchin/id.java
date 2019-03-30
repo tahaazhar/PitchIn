f7257465b3871c26a267cf78c7cbeca9271eaa7b
Taha Azhar <taha-azhar@hotmail.com>
2019-03-30 15:54:55 +0500

    Signup activities and xmls

---
+++ app/src/main/java/com/example/taha/pitchin/id.java
@@ -0,0 +1,13 @@
+package com.example.taha.pitchin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class id extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_id);
+    }
+}
